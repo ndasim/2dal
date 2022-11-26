9280dfcb282ca293000d24ce2b99c97f63d0288a
ndasim <asim.dogan.namli@gmail.com>
2022-11-26 15:11:05 +0300

    minor adjustment

--- main.go
+++ main.go
@@ -68,7 +68,9 @@ func main() {
 	router.Static("/static", "./static")
 	router.LoadHTMLFiles("static/forwarder.html", "static/index.html", "static/expired.html")
 	router.GET("/api/create", gin.Bind(shortener.CreateLinkStruct{}), shortener.CreateLink)
-	router.GET("/api/qr/:alias", shortener.CreateSvgQR)
+	
+	// SVG builder
+	router.GET("/qr/:alias", shortener.CreateSvgQR)
 	//router.GET("/api/qr/png/:alias", shortener.CreatePngQR)
 
 	router.GET("/:alias", func(ctx *gin.Context) {
