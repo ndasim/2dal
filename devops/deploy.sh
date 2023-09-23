c6cfbf338395db30a267d287e981ebae2e91d697
Asım Doğan NAMLI <asim.dogan.namli@gmail.com>
2023-09-23 23:40:36 +0300

    remote ssh issue

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,7 +1,22 @@
-aws configure set aws_access_key_id > echo $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key > echo $AWS_SECRET_ACCESS_KEY
-aws configure set default.region > echo eu-central-1
-aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 163166970182.dkr.ecr.eu-central-1.amazonaws.com
-sudo docker rm -f `sudo docker ps -q` 
-sudo docker run -d -p 80:80 $ECR_REGISTRY/2dal_container:latest
-```
+- name: Check out code
+        uses: actions/checkout@v2
+
+      - name: Configure AWS credentials
+        uses: aws-actions/configure-aws-credentials@v1
+        with:
+          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws-region: eu-central-1
+
+      - name: Login to Amazon ECR
+        id: login-ecr
+        uses: aws-actions/amazon-ecr-login@v1
+
+      - name: Build, tag, and push image to Amazon ECR
+        env:
+          ECR_REGISTRY: ${{ steps.login-ecr.outputs.registry }}
+          ECR_REPOSITORY: 2dal_container
+          IMAGE_TAG: ${{ github.sha }}
+        run: |
+          docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG .
+          docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
